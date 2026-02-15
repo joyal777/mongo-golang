5e4a92afeab4313322c126c18fbcd2af516c893c
joyal777 <joyaltony78@gmail.com>
2026-02-15 21:23:35 +0530

    First Commit

---
+++ controllers/user.go
@@ -0,0 +1,94 @@
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/joyal777/mongo-golang/models"
+	"github.com/julienschmidt/httprouter"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserController struct {
+	client *mongo.Client
+}
+
+func NewUserController(c *mongo.Client) *UserController {
+	return &UserController{c}
+}
+
+func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	id := ps.ByName("id")
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	u := models.User{}
+	coll := uc.client.Database("mongo-golang").Collection("users")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = coll.FindOne(ctx, bson.M{"_id": oid}).Decode(&u)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	uj, _ := json.Marshal(u)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s", uj)
+}
+
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	u := models.User{}
+	json.NewDecoder(r.Body).Decode(&u)
+
+	u.Id = primitive.NewObjectID()
+	coll := uc.client.Database("mongo-golang").Collection("users")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := coll.InsertOne(ctx, u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	uj, _ := json.Marshal(u)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "%s", uj)
+}
+
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	id := ps.ByName("id")
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	coll := uc.client.Database("mongo-golang").Collection("users")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err = coll.DeleteOne(ctx, bson.M{"_id": oid})
+	if err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Deleted user %s\n", id)
+}
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/joyal777/mongo-golang/controllers"
+	"github.com/julienschmidt/httprouter"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	r := httprouter.New()
+
+	// Get the modern MongoDB Client
+	client := getClient()
+
+	uc := controllers.NewUserController(client)
+
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+
+	fmt.Println("Server started at localhost:9000")
+	log.Fatal(http.ListenAndServe("localhost:9000", r))
+}
+
+func getClient() *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Connect using the official driver
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Ping to confirm connection
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Could not connect to MongoDB:", err)
+	}
+
+	fmt.Println("Successfully connected to MongoDB 8.0!")
+	return client
+}
